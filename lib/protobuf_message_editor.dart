a149204c3c875b29c31e7b561edcc0c5088cd928
vun <vunyunt@gmail.com>
2025-12-12 18:15:27 +0800

    Expose custom editor registry export

--- lib/protobuf_message_editor.dart
+++ lib/protobuf_message_editor.dart
@@ -1,3 +1,6 @@
 export 'src/proto_navigation_state.dart';
 export 'src/proto_message_editor.dart';
 export 'src/proto_dual_panel_message_editor.dart';
+export 'src/custom_editor_registry.dart';
+export 'src/utils/proto_field_type_extensions.dart';
+export 'src/utils/proto_message_extensions.dart';
